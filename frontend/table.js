7d8750b582d77db3beb7aaf0210793876b6b3d8b
sfhelmet <sfhelmet@gmail.com>
2023-05-04 21:15:49 -0400

    working dynamic table

--- frontend/table.js
+++ frontend/table.js
@@ -1,25 +1,41 @@
 import {API_KEY, SUMMONER_NAMES} from './config.js';
 
-async function getNames() {
+async function generateTable() {
     console.log(SUMMONER_NAMES)
-    let promises = [];
-    let result = [];
+    let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
+
 
     for (let i = 0; i < SUMMONER_NAMES.length; i++) {
         let name = SUMMONER_NAMES[i];
-        let url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?" + API_KEY
-        promises.push(fetch(url).then(response => {return response.json();}));
+        let endpoint = url + name + "?api_key=" + API_KEY
+        fetch(endpoint)
+          .then(response => response.json())
+          .then(data => {
+            const tableBody = document.getElementById('table-body');
+            const row = document.createElement('tr');
+            const col1 = document.createElement('td');
+            col1.textContent = data.name;
+            const col2 = document.createElement('td');
+            col2.textContent = data.summonerLevel;
+            const col3 = document.createElement('td');
+            col3.textContent = "Platinum 4"
+            const col4 = document.createElement('td');
+            col4.textContent = "0"
+            row.appendChild(col1);
+            row.appendChild(col2);
+            row.appendChild(col3);
+            row.appendChild(col4);
+            tableBody.appendChild(row);
+      });
     }
-
-    const data = await Promise.all(promises);
-    console.log(data)
     //     console.log(users);
     //     document.getElementById("demo").innerHTML = users.id;
     //     document.getElementById("fname").innerHTML = users.name;
     //     document.getElementById("flevel").innerHTML = users.summonerLevel;
     //     document.getElementById("ficon").innerHTML = users.profileIconId;
+    // });
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-    // getNames();
+    generateTable();
   });
